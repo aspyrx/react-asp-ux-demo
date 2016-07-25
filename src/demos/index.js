88513f6038fa1d61223ad78be6727839a3844ee7
Stan Zhang <stan.zhang2@gmail.com>
2016-07-24 23:35:11 -0700

    Add Select example

--- src/demos/index.js
+++ src/demos/index.js
@@ -3,8 +3,9 @@ import Lists from './Lists';
 import Links from './Links';
 import Buttons from './Buttons';
 import Inputs from './Inputs';
+import Selects from './Selects';
 
 export default [
-    Lists, Links, Buttons, Inputs
+    Lists, Links, Buttons, Inputs, Selects
 ].map((el, i) => React.createElement(el, { key: i }));
 
